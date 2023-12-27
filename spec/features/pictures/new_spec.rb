346ce8f3095eb3c0ca759a29c9c1086905a9bb7d
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-12-27 18:08:20 -0500

    Add test to check and fill out form

---
+++ spec/features/pictures/new_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'Pictures New Page', type: :feature do
+  it 'can see and fill out the form' do
+    visit('/pictures/new')
+
+    fill_in 'Title', with: 'Pic at Cancun'
+    fill_in 'Description', with: 'A pretty cool pic'
+    fill_in 'Type', with: 'landscape'
+    fill_in 'Device', with: 'iPhone 7'
+    fill_in 'Location', with: 'Cancun, Mexico'
+    fill_in 'Year', with: 2012
+
+    click_on 'Submit'
+
+    expect(current_path).to eq('/pictures')
+    expect(page).to have_content('Pic at Cancun')
+  end
+end
