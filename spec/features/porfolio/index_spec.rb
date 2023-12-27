efac6b206fbfc429c9d3dad366706646229ca2de
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-12-27 17:16:43 -0500

    Add test to show link to submit new pictures

--- spec/features/porfolio/index_spec.rb
+++ spec/features/porfolio/index_spec.rb
@@ -1,10 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Portfolio Index Page', type: :feature do
-
   it 'can see the porfolio message' do
     visit('/portfolio')
 
     expect(page).to have_content('This is my portfolio.')
+  end
+
+  it 'shows link to submit new picture' do
+    visit('/portfolio')
+
+    click_link ('Submit New Picture')
+    expect(current_path).to eq('/portfolio/new')
   end
 end
