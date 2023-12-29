0d086684b4bdca3d1d7e2ceb97e6c87ec7b9f7bb
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-12-29 13:38:19 -0500

    Add strong params

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -8,4 +8,9 @@ class PicturesController < ApplicationController
   def create
     binding.pry
   end
+
+  private
+  def pictures_params
+    params.permit(:title, :description, :type, :device, :location, :year)
+  end
 end
