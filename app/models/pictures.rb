2c63931f662e624858a086621caa121f26cdd973
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-12-29 13:50:49 -0500

    Add validation for 'type_of_picture'

--- app/models/pictures.rb
+++ app/models/pictures.rb
@@ -1,7 +1,7 @@
 class Pictures < ApplicationRecord
   validates_presence_of :title
   validates_presence_of :description
-  validates_presence_of :type
+  validates_presence_of :type_of_picture
   validates_presence_of :device
   validates_presence_of :location
   validates_presence_of :year
