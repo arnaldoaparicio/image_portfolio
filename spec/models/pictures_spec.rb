87bc3d522b308651b32a59568336bb23931e2e18
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-12-29 15:59:39 -0500

    Change attribute name from 'type' to 'type_of_picture'

--- spec/models/pictures_spec.rb
+++ spec/models/pictures_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Pictures, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:description) }
-    it { should validate_presence_of(:type) }
+    it { should validate_presence_of(:type_of_picture) }
     it { should validate_presence_of(:device) }
     it { should validate_presence_of(:location) }
     it { should validate_presence_of(:year) }
