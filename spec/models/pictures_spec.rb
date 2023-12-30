a6bdd8afbf2d562ceb0b6705cedede1ed4d78cd2
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-12-29 20:36:48 -0500

    Add test to check if image is valid

--- spec/models/pictures_spec.rb
+++ spec/models/pictures_spec.rb
@@ -9,4 +9,20 @@ RSpec.describe Pictures, type: :model do
     it { should validate_presence_of(:location) }
     it { should validate_presence_of(:year) }
   end
+
+  it 'is valid if image is attached' do
+    file = Rails.root.join('spec', 'test_image', 'image1.jpg')
+    image = ActiveStorage::Blob.create_and_upload!(io: File.open(file, 'rb'), filename: 'image1.jpg', content_type: 'image/jpeg').signed_id
+
+    picture_params = { title: 'Pic at Cancun',
+                       description: 'A pretty cool pic',
+                       type_of_picture: 'landscape',
+                       device: 'iPhone 7',
+                       location: 'Cancun, Mexico',
+                       year: 2012,
+                       personal_pic: image }
+    picture = Pictures.create!(picture_params)
+
+    expect(picture.personal_pic.valid?).to eq(true)
+  end
 end
