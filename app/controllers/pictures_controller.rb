67fe0667adfd9fc162b76f80b7ab52ac3fdee324
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-12-29 20:57:00 -0500

    Add 'personal_pic' attribute to strong params

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -13,6 +13,6 @@ class PicturesController < ApplicationController
 
   private
   def pictures_params
-    params.permit(:title, :description, :type_of_picture, :device, :location, :year)
+    params.permit(:title, :description, :type_of_picture, :device, :location, :year, :personal_pic)
   end
 end
