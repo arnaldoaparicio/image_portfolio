b74233fe291ee2087bccbf9fec14734701638835
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-01-10 17:42:15 -0500

    Change schema for 'Picture' table

---
+++ db/migrate/20231219200230_create_pictures.rb
@@ -0,0 +1,14 @@
+class CreatePictures < ActiveRecord::Migration[7.0]
+  def change
+    create_table :pictures do |t|
+      t.string :title
+      t.string :description
+      t.string :type_of_picture
+      t.string :device
+      t.string :location
+      t.integer :year
+
+      t.timestamps
+    end
+  end
+end
