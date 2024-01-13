7a8a925e137bed58805a2e1d5721d69a3fedc8ed
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-01-13 14:25:22 -0500

    Add test for 'all_landscapes' method

--- spec/models/picture_spec.rb
+++ spec/models/picture_spec.rb
@@ -25,4 +25,41 @@ RSpec.describe Picture, type: :model do
 
     expect(picture.personal_pic.valid?).to eq(true)
   end
+
+  it "selects all pictures in the 'animal' category" do
+    image1 = Picture.create!(title: 'Sunset in Mexico',
+                    description: "This is a picture of the sunset near my grandfather's house.",
+                    type_of_picture: 'landscape',
+                    device: 'iPhone 7',
+                    location: 'Puebla, Mexico',
+                    year: 2017)
+
+    image2 = Picture.create!(title: 'Baby Charlie',
+                    description: "A picture of baby Charlie being held like a bottle of water.",
+                    type_of_picture: 'animal',
+                    device: 'Sony a6100',
+                    location: 'Somewhere in New Jersey',
+                    year: 2022)
+
+    image3 = Picture.create!(title: 'East Point Shore',
+                    description: "Took a day trip with my partner at the East Point Lighthouse and decided to take a picture by the shore.",
+                    type_of_picture: 'landscape',
+                    device: 'iPhone SE (3rd Gen.)',
+                    location: 'Maurice River, NJ',
+                    year: 2022)
+
+    image4 = Picture.create!(title: 'Crabbing by the River',
+                    description: "A few hours into crabbing since 3am that morning. It was about 9am and decided to take a picture of the river.",
+                    type_of_picture: 'landscape',
+                    device: 'iPhone XS Max',
+                    location: 'Maurice River, NJ',
+                    year: 2019)
+
+    landspaces = Picture.all_landscapes
+
+    expect(landscapes.count).to eq(3)
+    landscapes.each do |landscape|
+      expect(landscape.type_of_picture).to eq('landscape')
+    end
+  end
 end
