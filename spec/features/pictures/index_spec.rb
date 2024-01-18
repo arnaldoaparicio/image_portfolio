92ca58fa8a25f10828ca2c914b254804380f436b
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-01-18 15:38:04 -0500

    Add 'before(:each)' to populate page with instances of 'Picture'

--- spec/features/pictures/index_spec.rb
+++ spec/features/pictures/index_spec.rb
@@ -1,7 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe 'Picture Index Page', type: :feature do
-  xit 'shows link to submit new picture' do
+  before(:each) do
+    file = Rails.root.join('spec', 'test_image', 'image1.jpg')
+    picture = ActiveStorage::Blob.create_and_upload!(io: File.open(file, 'rb'), filename: 'image1.jpg', content_type: 'image/jpeg').signed_id
+
+    image1 = Picture.create!(title: 'Sunset in Mexico',
+                description: "This is a picture of the sunset near my grandfather's house.",
+                type_of_picture: 'landscape',
+                device: 'iPhone 7',
+                location: 'Puebla, Mexico',
+                year: 2017,
+                personal_pic: picture)
+
+    image2 = Picture.create!(title: 'Baby Charlie',
+                description: "A picture of baby Charlie being held like a bottle of water.",
+                type_of_picture: 'animal',
+                device: 'Sony a6100',
+                location: 'Somewhere in New Jersey',
+                year: 2022,
+                personal_pic: picture)
+
+    image3 = Picture.create!(title: 'East Point Shore',
+                description: "Took a day trip with my partner at the East Point Lighthouse and decided to take a picture by the shore.",
+                type_of_picture: 'landscape',
+                device: 'iPhone SE (3rd Gen.)',
+                location: 'Maurice River, NJ',
+                year: 2022,
+                personal_pic: picture)
+
+    image4 = Picture.create!(title: 'Crabbing by the River',
+                description: "A few hours into crabbing since 3am that morning. It was about 9am and decided to take a picture of the river.",
+                type_of_picture: 'landscape',
+                device: 'iPhone XS Max',
+                location: 'Maurice River, NJ',
+                year: 2019,
+                personal_pic: picture)
+
+    image5 = Picture.create!(title: 'Tippy Tina Sleeping',
+                description: "Decided to visit my friend Helga and wanted to take a picture of her house pig, Tippy Tina.",
+                type_of_picture: 'animal',
+                device: 'iPhone 8',
+                location: 'Hopewell, NJ',
+                year: 2023,
+                personal_pic: picture)
+
+    image6 = Picture.create!(title: 'Three Puppers',
+                description: "Worked a day at Gardie's Doggy Day Camp and captured this precious picture of three sisters.",
+                type_of_picture: 'animal',
+                device: 'iPhone XR',
+                location: 'Elmer, NJ',
+                year: 2023,
+                personal_pic: picture)
+  end
+  
+  it 'shows link to submit new picture' do
     visit('/pictures')
 
     expect(page).to have_link('Submit New Picture')
@@ -10,7 +63,7 @@ RSpec.describe 'Picture Index Page', type: :feature do
     expect(current_path).to eq('/pictures/new')
   end
 
-  xit 'shows categories of pictures' do
+  it 'shows categories of pictures' do
     visit('/pictures')
 
     expect(page).to have_content('Animals')
