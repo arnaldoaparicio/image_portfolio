900c1163e321d01707c6a4262917880d148fd92c
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-01-18 15:11:43 -0500

    Add 'show' method

--- app/controllers/animal_pictures_controller.rb
+++ app/controllers/animal_pictures_controller.rb
@@ -1,5 +1,10 @@
 class AnimalPicturesController < ApplicationController
+  include ActiveStorage::SetCurrent
   def index
     @animal_pictures = Picture.all_animals
+  end
+
+  def show
+    @animal_picture = Picture.find(params[:id])
   end
 end
